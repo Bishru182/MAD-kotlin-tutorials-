a1c36b2d3bebd5a1d06279923bb793e1c328e84c
Bishru182 <bishrumohammed182@gmail.com>
2025-02-24 12:00:10 +0530

    declairing functions

--- functions-tute1/src/Main.kt
+++ functions-tute1/src/Main.kt
@@ -1,5 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun printAge(age:Int, name:String){
+    println("$name is $age years old")
+}
+
 fun main(){
-    println("bish")
+    printAge(12,"sam")
 }
