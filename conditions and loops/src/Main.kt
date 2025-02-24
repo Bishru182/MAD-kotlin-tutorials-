f1705b07bf1b7c8a455b2651efe0c247c9a97075
Bishru182 <bishrumohammed182@gmail.com>
2025-02-24 17:15:16 +0530

    conditions and loop-tute01

---
+++ conditions and loops/src/Main.kt
@@ -0,0 +1,66 @@
+/*val a=1
+val c=2
+
+fun main(){
+    if(a<c){
+        println("a is the smallest")
+    }else{
+        println("c is the smallest")
+    }
+}*/
+
+/*val x=6
+fun main(){
+   when(x) {
+       0, 1 -> println("x is 0 or 1")
+       6,9 -> println("x is 6 or 9")
+       else -> println("otherwise")
+   }
+}*/
+
+/*fun main(){
+    for(i in 1..10){
+        println(i)
+
+    }
+    println("?//////////////////////////0")
+
+    for(i in 1..10 step 5){
+        println(i)
+
+    }
+    println("///////////////////////////////")
+
+    for (i in 1 until 5){
+        println(i)
+    }
+}*/
+
+/*fun main(){
+    for(i in 1..10){
+        println(i)
+        if(i==5){
+            break;
+
+        }
+    }
+}*/
+
+/*fun main(){
+    for(i in 1..10){
+        if(i==5){
+            continue
+        }
+        println(i)
+    }
+}*/
+
+/*var i=0
+fun main(){
+    while (i<=5){
+        println(i)
+        i++
+    }
+}*/
+
+
